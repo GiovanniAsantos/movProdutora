4c5ea10f82901cc51ca020a2d28f459524146bc1
moisesgui <mgoliveira.dev@gmail.com>
2024-08-07 08:28:07 -0300

    set chakra default theme preferences

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import "./App.css";
-import HomePage from "./pages/HomePage";
+import { ChakraProvider } from '@chakra-ui/react'
+import './App.css'
+import HomePage from './pages/HomePage'
+import customTheme from './theme/theme'
+import { Global } from '@emotion/react'
 
 function App() {
   return (
-    <>
+    <ChakraProvider theme={customTheme}>
       <HomePage />
-    </>
-  );
+    </ChakraProvider>
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
+import theme from './theme/theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
