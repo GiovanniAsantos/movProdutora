22786ff5557d39fa8aecd9e60f788d9cb53b6ad2
moisesgui <mgoliveira.dev@gmail.com>
2024-08-07 21:29:18 -0300

    routes config created

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
-import theme from './theme/theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 )
