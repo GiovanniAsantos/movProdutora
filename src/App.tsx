c46fc86152f5ad4a19916b1885028b1e9dd26425
moisesgui <mgoliveira.dev@gmail.com>
2024-08-07 22:57:45 -0300

    tsconfig update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import './App.css'
-import AppRoutes from './routes'
-import customTheme from './theme/theme'
+import AppRoutes from '@/routes'
+import customTheme from '@/theme/theme'
 
 function App() {
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 // src/services/routes/Routes.tsx
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import routesConfig from './routesConfig'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import routesConfig from '@/routes/routesConfig'
 
 const AppRoutes: React.FC = () => (
   <Router>
