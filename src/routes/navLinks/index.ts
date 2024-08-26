b8bf8cdf18ddd74ff0de676bd5f63c7cd8bcbf8d
Giovanni Azevedo <giovanniadev@gmail.com>
2024-08-26 17:44:40 -0300

    About pagr skeleton done. Component Card created.

--- src/routes/navLinks/index.ts
+++ src/routes/navLinks/index.ts
@@ -3,7 +3,7 @@ import { NavLinkRouter } from '../routes.dto'
 export const navLinks: NavLinkRouter[] = [
   { name: 'Home', href: '/' },
   { name: 'Sobre', href: '/about' },
-  { name: 'Contato', href: '/contact' },
+  { name: 'Portifólio', href: '/contact' },
   { name: 'Clientes', href: '/clients' },
-  { name: 'Eventos', href: '/events' }
+  { name: 'Orçamento', href: '/events' }
 ]
