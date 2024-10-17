e1c7239d324ad0348026787902330bc6b8e21546
moisesgui <mgoliveira.dev@gmail.com>
2024-10-17 19:44:49 -0300

    :package: update packages json

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
-
-// Correct way to create __dirname in an ES module environment
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // A pasta de saída deve ser configurada corretamente
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') // Correct alias path resolution
-    }
-  },
-  server: {
-    watch: {
-      usePolling: true // Enable polling to watch for file changes
+      '@': '/src' // Use alias adequadamente
     }
   }
 })
